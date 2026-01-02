637531de0e5daecb65ba8ff3792dfd8de3ac6229
tdefuria <tdefuria@gmail.com>
2026-01-01 20:54:22 -0500

    home page redirect

--- app/(kambaz)/page.tsx
+++ app/(kambaz)/page.tsx
@@ -1,12 +1,4 @@
-export default function Lab1() {
-    return (
-        <div id="wd-landing_page">
-            <h2>Landing Page</h2>
-            <h3>Choices</h3>
-            <div id="wd-h-tag">
-                <h4>Lab 1</h4>
-This will be a hyperlink to the Lab 1 page.
-            </div>
-            {/* do next exercise here */}
-        </div>
-);}
+import { redirect } from "next/navigation";
+export default function Kambaz() {
+    redirect("/Labs/Lab1");
+}
